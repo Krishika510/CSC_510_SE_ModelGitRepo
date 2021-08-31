909fa04e5effc0aa1b2a8a0dda9a167a9e692180
apurva-s <32777604+apurva-s@users.noreply.github.com>
2021-08-30 22:51:57 -0400

    Update test_init.py

--- test/test_init.py
+++ test/test_init.py
@@ -12,16 +12,22 @@ def test_fibonacci():
     assert fibonacci(10) == 55
 
 def test_factorial():
-    assert factorial(-1) == -1
-    assert factorial(20) == -1
-    assert factorial(4) == 24
+    assert factorial(-1) == -1,"Negative numbers should yield -1 for all, they do not have a factorial"
+	assert factorial(-2) == -1,"Negative numbers should yield -1 for all, they do not have a factorial"
+	assert factorial(0) == 1,"Factorial of 0 or 1 should be 1, the output is something else"
+	assert factorial(1) == 1,"Factorial of 0 or 1 should be 1, the output is something else"
+	assert factorial(2) == 2,"Factorial of 2 is 2, the output is something else"
+    assert factorial(20) == -1,"Anything above 10 is -1, we have a restricted range"
+    assert factorial(4) == 24,"Factorial of 4 is 24, the output is something else"
 
 def test_isPrime():
-    assert isPrime(-1) == False
-    assert isPrime(1) == False
-    assert isPrime(2) == True
-    assert isPrime(10) == False
-    assert isPrime(11) == True
+    assert isPrime(-1) == False,"-1 is neither prime nor composite"
+	assert isPrime(0) == False,"0 is neither prime nor composite "
+    assert isPrime(1) == False,"1 is neither prime, nor composite"
+    assert isPrime(2) == True,"2 is neither prime, nor composite"
+    assert isPrime(10) == False,"10 is a composite number, the output is wrong"
+    assert isPrime(11) == True,"11 is a prime number, the output is wrong"
+    assert isPrime(13) == True,"13 is a prime number, the output is wrong"
 
 def test_prime():
     assert prime(-1) == -1
